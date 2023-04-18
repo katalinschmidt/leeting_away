bdf46f537dfcd9e90d9798b29e5061ff665d5a10
katalinschmidt <katalin.schmidt.97@gmail.com>
2023-04-18 12:35:33 -0400

    Compress isValid solution

--- stacks.py
+++ stacks.py
@@ -6,47 +6,37 @@ class Solution:
             20. Valid Parentheses
             https://leetcode.com/problems/valid-parentheses/
         """
-        # Things I know:
-        # I must validate my str 1) contains chars and 2) each char is a valid parantheses
-        # To be (potentially) balanced, the str length must be an even number
-        # Rule of thumb - Balancing / reversing / validity requires a stack (list in Python), LIFO
-        # I can add opening brackets to the stack, then pop off the stack once I encounter a closing bracket to see if they are the same type. E.g. "({})" -> Pop "{" once I see "}" & compare.
-        # I will need to define my opening & closing bracket types for the computer
-
-        # Confirm the str length is even
+        # Odd str length immediately signals unbalanced parantheses
         if len(s) % 2 != 0:
             return False
 
         # Create the stack
         stack = []
 
+        # Define valid brackets
+        opening_brackets = ["(", "{", "["]
+        bracket_pairings = {
+            "(": ")",
+            "{": "}",
+            "[": "]"
+        }
+
         # Examine each char in the str
         for c in s:
-            # If the char is an opening bracket, add it to the stack
-            if c in ["(", "{", "["]:
-                stack.append(c)
+            # If the char is an opening bracket, add it's closing equivalent to the stack
+            if c in opening_brackets:
+                stack.append(bracket_pairings[c])
             # If the char is a closing bracket, compare it to the last opening bracket seen / last el added to the stack (LIFO)
+            # Req: Make sure we've already seen an opening bracket, otherwise we're looking at a case of closing brackets first / only
+            elif stack and stack[-1] == c:
+                # If they match, remove the bracket from the stack, so we can successfully continue examining the rest of the string
+                stack.pop()
             else:
-                # Make sure we've already seen an opening bracket, otherwise we're looking at a case of closing brackets first / only
-                if len(stack) == 0:
-                    return False
-
-                # If the brackets are not a match, return because we know our str is not balanced
-                last_open = stack.pop()
-                if last_open == "(" and c != ")":
-                    return False
-                if last_open == "{" and c != "}":
-                    return False
-                if last_open == "[" and c != "]":
-                    return False
-
-                # Otherwise, continue examining the str
+                return False
 
         # Make sure we've exhausted the stack, otherwise we're looking at a case of only open brackets
-        if len(stack) != 0:
-            return False
+        return len(stack) == 0
 
-        return True
 
 class TestStackSolutions(TestCase):
     def testIsValid(self):
