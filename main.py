a6006c57e7d56b013a3641969c862763c09ab5b7
katalinschmidt <katalin.schmidt.97@gmail.com>
2023-04-18 12:23:53 -0400

    Solve valid parantheses

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
+from stacks import TestStackSolutions
 
 def execute():
     problem_num = input("Which problem number would you like test? ")
-    print(f"OK, executing problem number {problem_num}.")
+    print(f"\nOK, executing problem number {problem_num}.\n")
+
+    if problem_num in ["20"]:
+        test = TestStackSolutions()
+
+        if problem_num == "20":
+            test.testIsValid()
 
 
 if __name__ == "__main__":
--- stacks.py
+++ stacks.py
@@ -1,10 +1,89 @@
-# 20. Valid Parentheses
-# https://leetcode.com/problems/valid-parentheses/
+from unittest import TestCase
 
 class Solution:
     def isValid(self, s: str) -> bool:
-        pass
+        """
+            20. Valid Parentheses
+            https://leetcode.com/problems/valid-parentheses/
+        """
+        # Things I know:
+        # I must validate my str 1) contains chars and 2) each char is a valid parantheses
+        # To be (potentially) balanced, the str length must be an even number
+        # Rule of thumb - Balancing / reversing / validity requires a stack (list in Python), LIFO
+        # I can add opening brackets to the stack, then pop off the stack once I encounter a closing bracket to see if they are the same type. E.g. "({})" -> Pop "{" once I see "}" & compare.
+        # I will need to define my opening & closing bracket types for the computer
 
-class Test:
+        # Confirm the str length is even
+        if len(s) % 2 != 0:
+            return False
+
+        # Create the stack
+        stack = []
+
+        # Examine each char in the str
+        for c in s:
+            # If the char is an opening bracket, add it to the stack
+            if c in ["(", "{", "["]:
+                stack.append(c)
+            # If the char is a closing bracket, compare it to the last opening bracket seen / last el added to the stack (LIFO)
+            else:
+                # Make sure we've already seen an opening bracket, otherwise we're looking at a case of closing brackets first / only
+                if len(stack) == 0:
+                    return False
+
+                # If the brackets are not a match, return because we know our str is not balanced
+                last_open = stack.pop()
+                if last_open == "(" and c != ")":
+                    return False
+                if last_open == "{" and c != "}":
+                    return False
+                if last_open == "[" and c != "]":
+                    return False
+
+                # Otherwise, continue examining the str
+
+        # Make sure we've exhausted the stack, otherwise we're looking at a case of only open brackets
+        if len(stack) != 0:
+            return False
+
+        return True
+
+class TestStackSolutions(TestCase):
     def testIsValid(self):
-        pass
+        test_data = [
+            # Standard tests
+            {
+                "input": "()",
+                "output": True
+            },
+            {
+                "input": "()[]{}",
+                "output": True
+            },
+            {
+                "input": "(]",
+                "output": False
+            },
+            # Edge cases
+            {
+                "input": "(()))",
+                "output": False
+            },
+            {
+                "input": "((",
+                "output": False
+            },
+            {
+                "input": "){",
+                "output": False
+            },
+        ]
+
+        solution = Solution()
+        for test in test_data:
+            print(f"""Testing input "{test['input']}".""")
+            try:
+                self.assertEqual(solution.isValid(test["input"]), test["output"])
+            except:
+                print("Solution failed!\n")
+
