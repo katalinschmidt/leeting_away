8f6197281fb6a7f050805e2b4ac7875608b76ff2
katalinschmidt <katalin.schmidt.97@gmail.com>
2023-04-18 10:48:07 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+def execute():
+    problem_num = input("Which problem number would you like test? ")
+    print(f"OK, executing problem number {problem_num}.")
+
+
+if __name__ == "__main__":
+    execute()
---
+++ stacks.py
@@ -0,0 +1,10 @@
+# 20. Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses/
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pass
+
+class Test:
+    def testIsValid(self):
+        pass
