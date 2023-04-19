0dda2635da2b023d7f5b58e241f8847506563aa9
katalinschmidt <katalin.schmidt.97@gmail.com>
2023-04-19 15:54:26 -0400

    Solve inorder traversal

--- main.py
+++ main.py
@@ -1,14 +1,23 @@
 from stacks import TestStackSolutions
 
+STACK_PROBLEMS = ["20", "94"]
+
 def execute():
     problem_num = input("Which problem number would you like test? ")
     print(f"\nOK, executing problem number {problem_num}.\n")
 
-    if problem_num in ["20"]:
+    if problem_num in STACK_PROBLEMS:
         test = TestStackSolutions()
 
         if problem_num == "20":
             test.testIsValid()
+        elif problem_num == "94":
+            test.testInorderTraversal()
+        else:
+            print("Sorry, this problem number hasn't been solved yet.\n")
+
+    else:
+        print("Sorry, this problem number hasn't been solved yet.\n")
 
 
 if __name__ == "__main__":
--- stacks.py
+++ stacks.py
@@ -1,10 +1,13 @@
+from typing import Optional, List
 from unittest import TestCase
+from utils import TreeNode
+
 
 class Solution:
     def isValid(self, s: str) -> bool:
         """
-            20. Valid Parentheses
-            https://leetcode.com/problems/valid-parentheses/
+        20. Valid Parentheses
+        https://leetcode.com/problems/valid-parentheses/
         """
         # Odd str length immediately signals unbalanced parantheses
         if len(s) % 2 != 0:
@@ -36,6 +39,46 @@ class Solution:
 
         # Make sure we've exhausted the stack, otherwise we're looking at a case of only open brackets
         return len(stack) == 0
+
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        """
+        94. Binary Tree Inorder Traversal
+        https://leetcode.com/problems/binary-tree-inorder-traversal/
+        """
+        # Things I know:
+        # Inorder tree traversal == visit left child until there is no more left child / unvisited left child, then move back to root and visit right child & repeat
+        # Stack can help keep track of nodes we need to go back to
+        # Inputs that can complicate things are
+        # Empty trees
+        # Single node trees
+        # Balanced vs. skewed trees - trees with only side (L or R)
+        # Complete vs. incomplete trees
+
+        # Create the stack & our results list
+        stack = []
+        result = []
+
+        # Begin at the provided root
+        current_node = root
+        # Traverse until we've exhausted all nodes
+        while current_node or stack:
+            # Left traversal:
+            while current_node:
+                # Keep track of where we've been
+                stack.append(current_node)
+                # Traverse to left child
+                current_node = current_node.left
+
+            # Right traversal:
+            # Once we've exhausted left children, return to root (last el in stack)
+            current_node = stack.pop()
+            # Save the value for our results list
+            if current_node.val is not None:
+                result.append(current_node.val)
+            # Traverse to right child
+            current_node = current_node.right
+
+        return result
 
 
 class TestStackSolutions(TestCase):
@@ -72,8 +115,57 @@ class TestStackSolutions(TestCase):
         solution = Solution()
         for test in test_data:
             print(f"""Testing input "{test['input']}".""")
+            response = solution.isValid(test["input"])
             try:
-                self.assertEqual(solution.isValid(test["input"]), test["output"])
+                self.assertEqual(response, test["output"])
             except:
-                print("Solution failed!\n")
+                print(f"Solution failed! {response} != {test['output']} \n")
 
+    def testInorderTraversal(self):
+        # [1, None, 2, 3]
+        tree1 = TreeNode(1)
+        tree1.right = TreeNode(2)
+        tree1.right.left = TreeNode(3)
+
+        # []
+        tree2 = TreeNode(None)
+
+        # [1]
+        tree3 = TreeNode(1)
+
+        # [1, 2, None, 3, None]
+        tree4 = TreeNode(1)
+        tree4.left = TreeNode(2)
+        tree4.right = None
+        tree4.left.left = TreeNode(3)
+        tree4.left.right = None
+
+        test_data = [
+            # Standard tests
+            {
+                "input": tree1,
+                "output": [1, 3, 2]
+            },
+            {
+                "input": tree2,
+                "output": []
+            },
+            {
+                "input": tree3,
+                "output": [1]
+            },
+            # Edge cases
+            {
+                "input": tree4,
+                "output": [3, 2, 1]
+            },
+        ]
+
+        solution = Solution()
+        for test in test_data:
+            print(f"Testing input tree root {test['input'].val}.")
+            response = solution.inorderTraversal(test["input"])
+            try:
+                self.assertEqual(response, test["output"])
+            except:
+                print(f"Solution failed! {response} != {test['output']} \n")
